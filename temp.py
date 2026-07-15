2554ec10f2be86569e4eb732e02f76b4e894d941
Hao Nhut Vo <nhuthaolv2929@gmail.com>
2026-07-15 22:20:10 +0700

    chore: remove debug scripts

--- temp.py
+++
@@ -1,4 +0,0 @@
-import json
-for c in json.load(open('output_full.json', encoding='utf-8')):
- if c['token_count'] > 200:
-  print(f"Length {c['token_count']}: {c['text'][:100]}...")
--- temp4.py
+++
@@ -1,9 +0,0 @@
-﻿import json
-import sys
-import re
-sys.path.append('src')
-from woodpecker.core.chunker import chunk_with_overlap, estimate_tokens, split_by_sentences, _split_long_sentence
-text = [c['text'] for c in json.load(open('output_full.json', encoding='utf-8')) if c['token_count'] == 399][0]
-print(f'Sentences: {len(split_by_sentences(text))}')
-print(f'Subsentences: {len(_split_long_sentence(text, 200))}')
-
